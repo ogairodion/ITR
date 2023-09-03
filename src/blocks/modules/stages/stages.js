9a6bc771a5364782198e8347ba1e0f0bc7843c70
ogairodion <ogairodion@gmail.com>
2023-09-03 07:57:10 +0300

    slide steps

--- src/blocks/modules/stages/stages.js
+++ src/blocks/modules/stages/stages.js
@@ -8,6 +8,7 @@ const slidesCountCurrent = parent.querySelector('.slider-navigation__numbers-cur
 const slidesCount = parent.querySelector('.slider-navigation__numbers-all');
 const buttonMore = parent.querySelector('.button-show-more');
 const sliderWrapper = parent.querySelector('.stages__slider');
+const stagesSteps = parent.querySelectorAll('.stages__step');
 
 let visibleCount = 3;
 
@@ -27,15 +28,38 @@ const stagesSlider = new Swiper('.stages__slider', {
     },
 });
 
+const slideFirstHeight = stagesSlider.slides[0].clientHeight;
+const slideLastHeight = stagesSlider.slides[stagesSlider.slides.length - 1].clientHeight + slideFirstHeight;
+
+if (stagesSteps.length && stagesSlides.length) {
+    getStepPosition(0);
+}
+
 slidesCount.innerText = stagesSlides.length / visibleCount;
 
 if (stagesSlides.length) {
+    let firstStep = 1;
+
     stagesSlider.on('slideChange', () => {
-        slidesCountCurrent.innerText = stagesSlider.realIndex + 1;
+        const page = stagesSlider.realIndex + 1;
+
+        if (stagesSlider.previousIndex < stagesSlider.realIndex) {
+            firstStep += 3;
+        } else {
+            firstStep -= 3;
+        }
+
+        slidesCountCurrent.innerText = page;
 
         if (!buttonMore.classList.contains('hidden')) {
             buttonMore.classList.add('hidden');
         }
+
+        stagesSteps[0].innerText = `Шаг ${firstStep >= 10 ? firstStep : `0${firstStep}`}`;
+        stagesSteps[1].innerText = `Шаг ${firstStep + 1 >= 10 ? firstStep + 1 : `0${firstStep + 1}`}`;
+        stagesSteps[2].innerText = `Шаг ${firstStep + 2 >= 10 ? firstStep + 2 : `0${firstStep + 2}`}`;
+
+        getStepPosition(firstStep);
     });
 
     buttonMore.addEventListener('click', () => {
@@ -45,8 +69,29 @@ if (stagesSlides.length) {
 }
 
 
+function getStepPosition(number) {
+    let count = number === 1 ? 0 : number;
+
+    stagesSteps.forEach((step, index) => {
+        step.style.left = `${(count / stagesSlides.length) * 100}%`;
+        step.style.transform = `translateX(-${(count / stagesSlides.length) * 100}%)`;
+
+        if (index === 1) {
+            step.style.top = `${slideFirstHeight - (step.clientHeight / 2)}px`;
+        }
+    
+        if (index === 2) {
+            step.style.top = `${slideLastHeight - (step.clientHeight / 2)}px`;
+        }
+
+        count++;
+    });
+}
+
+
 function showMore(swiperWrapper) {
     visibleCount += 4;
+    const steps = document.querySelectorAll('.stages__slide-step');
 
     stagesSlides.forEach((stagesSlide, index) => {
         if (swiperWrapper) {
@@ -71,6 +116,19 @@ function showMore(swiperWrapper) {
 
         hideButton();
     });
+
+    stagesSteps.forEach((step) => {
+        step.remove();
+    });
+
+    for(let i = 0; i < visibleCount; i++) {
+        const count = i + 1 === 1 ? 0 : i + 1;
+        
+        steps[i].classList.remove('hidden');
+
+        steps[i].style.left = `${(count / stagesSlides.length) * 100}%`;
+        steps[i].style.transform = `translateX(-${(count / stagesSlides.length) * 100}%)`;
+    }
 }
 
 function hideButton() {
