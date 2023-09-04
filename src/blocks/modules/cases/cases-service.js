126bbc9ca5417bb04020de4554b1b41985cc7c90
ogairodion <ogairodion@gmail.com>
2023-09-04 09:38:40 +0300

    adaptive

--- src/blocks/modules/cases/cases-service.js
+++ src/blocks/modules/cases/cases-service.js
@@ -6,28 +6,44 @@ const casesSliders = document.querySelectorAll('.cases__item');
 const slidesCountCurrent = parent.querySelector('.slider-navigation__numbers-current');
 const slidesCount = parent.querySelector('.slider-navigation__numbers-all');
 
+let windowWidth = 0;
 let visibleCount = 4;
 let perGroup = 2;
 
+windowWidth = window.innerWidth
+
+slidesCount.innerText = windowWidth < 992 ? casesSliders.length: Math.ceil(casesSliders.length / visibleCount);
+
+window.addEventListener('resize', () => {
+    windowWidth = window.innerWidth;
+
+    slidesCount.innerText = windowWidth < 992 ? casesSliders.length: Math.ceil(casesSliders.length / visibleCount);
+    slidesCountCurrent.innerText = windowWidth < 992 ? casesSlider.activeIndex + 1 : Math.ceil((casesSlider.activeIndex / perGroup) + 1);
+});
+
 const casesSlider = new Swiper('.cases__items', {
     modules: [Navigation, Pagination, Grid],
-    slidesPerView: 2,
-    slidesPerGroup: 2,
+    slidesPerView: 1,
     spaceBetween: 16,
     resistance: 0,
     resistanceRation: false,
     speed: 1200,
-    grid: {
-        rows: 2,
-    },
     navigation: {
         nextEl: '.cases .slider-navigation__arrow-next',
         prevEl: '.cases .slider-navigation__arrow-prev',
     },
+    breakpoints: {
+        1200: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+            grid: {
+                rows: 2,
+            },
+        }
+    },
 });
 
-slidesCount.innerText = casesSliders.length / visibleCount;
 
 casesSlider.on('slideChange', () => {
-    slidesCountCurrent.innerText = (casesSlider.activeIndex / perGroup) + 1;
+    slidesCountCurrent.innerText = windowWidth < 992 ? casesSlider.activeIndex + 1 : Math.ceil((casesSlider.activeIndex / perGroup) + 1);
 });
